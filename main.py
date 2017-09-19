5a852cf09963bf47e0fb6f9bd062be2491933b05
yicheng <2396611405@qq.com>
2017-09-19 17:06:07 +0800

    for循环

---
+++ main.py
@@ -0,0 +1,13 @@
+#coding=utf-8
+#import first.py
+print "你好吗"
+print "hello python"
+
+"""变量定义的规则：
+    
+    变量名只能是 字母、数字或下划线的任意组合
+    变量名的第一个字符不能是数字
+    以下关键字不能声明为变量名
+    ['and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'exec', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'not', 'or', 'pass', 'print', 'raise', 'return', 'try', 'while', 'with', 'yield'] """
+name1 = "wupeiqi"
+print name1
