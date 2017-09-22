07e38ef1c4e085c16405d2c06b59c2515bb6397c
yicheng <2396611405@qq.com>
2017-09-22 15:20:45 +0800

    利率测试

--- first.py
+++ first.py
@@ -1,7 +1,11 @@
 #coding=utf-8
+
 # 注释  for循环
-for i in range(1,5):
-    for j in range(1,5):
-        for k in range(1,5):
-            if( i != k ) and (i != j) and (j != k):
-                print i,j,k
+#for i in range(1,5):
+#    for j in range(1,5):
+#        for k in range(1,5):
+#            if( i != k ) and (i != j) and (j != k):
+#                print i,j,k
+
+
+
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
 #coding=utf-8
 #import first.py
-print "你好吗"
-print "hello python"
+
+
+#print "你好吗"
+#print "hello python"
 
 """变量定义的规则：
-    
     变量名只能是 字母、数字或下划线的任意组合
     变量名的第一个字符不能是数字
     以下关键字不能声明为变量名
     ['and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'exec', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'not', 'or', 'pass', 'print', 'raise', 'return', 'try', 'while', 'with', 'yield'] """
 name1 = "wupeiqi"
 print name1
+name  = "China"
+print(name)
+#i = int(raw_input('ssss:'))
+##i = 600000
+#arr = [1000000,600000,400000,200000,100000,0]
+#rat = [0.01,0.015,0.03,0.05,0.075,0.1]
+#r = 0
+#for idx in range(0,6):
+#    if i>arr[idx]:
+#        r+=(i-arr[idx])*rat[idx]
+#        print (i-arr[idx])*rat[idx]
+#        i=arr[idx]
+#print r
+
